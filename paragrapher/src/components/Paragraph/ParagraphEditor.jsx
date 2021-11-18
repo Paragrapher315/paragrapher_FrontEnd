3e2c2fa02dbe47805ec8a2c55a3a0076f8b16484
kiaksar <kiaksar.shirvani.m@gmail.com>
2021-11-18 20:31:29 +0330

    grids added

--- paragrapher/src/components/Paragraph/ParagraphEditor.jsx
+++ paragrapher/src/components/Paragraph/ParagraphEditor.jsx
@@ -3,6 +3,7 @@ import {
   TextField,
   Typography,
   Switch,
+  Grid,
   List,
   ListItem,
   ListItemIcon,
@@ -11,26 +12,57 @@ import {
 class ProfileEditor extends React.Component {
   render() {
     return (
-      <div style={{ padding: "10vh 10vw" }}>
+      <div style={{ padding: "10vh 10vw", width: "70%" }}>
         <form>
-          <Typography>نوع پاراگراف: </Typography>
-          <Typography style={{ display: "inline" }}>شخصی</Typography>
-          <Switch />
-          <Typography style={{ display: "inline" }}>کتابی</Typography>
-          <br />
-          <TextField
-            label="نام کتاب"
-            style={{ direction: "rtl", textAlign: "right" }}
-            inputProps={{ textAlign: "right" }}
-          />
-          <TextField label="نام نویسنده" />
-          <br />
-          <TextField
-            label="متن پاراگراف"
-            multiline
-            rows={4}
-            style={{ width: "50%" }}
-          />
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Grid container>
+                <Grid item lg={6} md={6} xs={12}>
+                  <Typography>نوع پاراگراف: </Typography>
+                </Grid>
+                <Grid item lg={6} md={6} xs={12}>
+                  <Grid container>
+                    <Grid item lg={3} md={3} xs={3}>
+                      <Typography>کتابی</Typography>
+                    </Grid>
+                    <Grid item lg={3} md={3} xs={3}>
+                      <Switch />
+                    </Grid>
+                    <Grid item lg={3} md={3} xs={3}>
+                      <Typography>شخصی</Typography>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={12}>
+              <Grid container spacing={2}>
+                <Grid item lg={6} md={6} xs={12}>
+                  <TextField
+                    label="نام کتاب"
+                    style={{
+                      direction: "rtl",
+                      textAlign: "right",
+                      width: "100%",
+                    }}
+                    inputProps={{ textAlign: "right" }}
+                  />
+                </Grid>
+                <Grid item lg={6} md={6} xs={12}>
+                  <TextField label="نام نویسنده" style={{ width: "100%" }} />
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="متن پاراگراف"
+                multiline
+                rows={4}
+                style={{ width: "100%" }}
+              />
+            </Grid>
+            <Grid item xs={12}></Grid>
+          </Grid>
         </form>
       </div>
     );
