9148726134a511d4f0cfeb7271e8a3871ea10dc3
Hediyeh-Eshaqi <hediyeh.eshaqi@gmail.com>
2021-11-20 21:49:49 +0330

    Main structure is ready

---
+++ paragrapher/src/components/CreateComunity/CreateComunity.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import CreateComunityPic1 from "../../assets/CreateComunityPic1.png";
+import CreateComunityPic2 from "../../assests/CreateComunityPic2.svg"
+class CreateComunity extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    };
+  }
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return (
+        <div className="container">
+            <div className="row">
+                
+            </div>
+            <div className="row align-items-center">
+                
+            </div>
+            <div className="row">
+               
+            </div>
+            <div className="row align-items-center">
+                
+            </div>
+        </div>
+    );
+  }
+}
+export default CreateComunity;
+
