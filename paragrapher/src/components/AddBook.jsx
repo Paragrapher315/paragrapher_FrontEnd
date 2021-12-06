bf6c7f78dd14ee2541854152ff13d69eca8d48ca
kiaksar <kiaksar.shirvani.m@gmail.com>
2021-12-06 15:27:32 +0330

    text fields added and basic grids added

--- paragrapher/src/components/AddBook.jsx
+++ paragrapher/src/components/AddBook.jsx
@@ -13,12 +13,13 @@ import {
   ListItemIcon,
   ListItemText,
   Card,
+  Button,
   Avatar,
 } from "@material-ui/core";
 class AddBook extends React.Component {
   render() {
     return (
-      <Card>
+      <Card style={{ width: "75vw", padding: "1vh 1vw" }}>
         <form>
           <Grid container>
             <Grid item xs={12} lg={6} md={6}>
@@ -32,12 +33,77 @@ class AddBook extends React.Component {
                 }}
               ></div>
             </Grid>
-            <Grid item xs={12} lg={6} md={6}>
-              <TextField />
-              <br />
-              <TextField />
-              <br />
-              <TextField />
+            <Grid
+              item
+              xs={12}
+              lg={6}
+              md={6}
+              style={{ padding: "0 1vw", margin: "auto" }}
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="filled"
+                    label="نام کتاب"
+                    style={{
+                      direction: "rtl",
+                      textAlign: "right",
+                      width: "100%",
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="filled"
+                    label="نام نویسنده"
+                    style={{
+                      direction: "rtl",
+                      textAlign: "right",
+                      width: "100%",
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="filled"
+                    label="ژانر"
+                    style={{
+                      direction: "rtl",
+                      textAlign: "right",
+                      width: "100%",
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="filled"
+                    label="قیمت"
+                    style={{
+                      direction: "rtl",
+                      textAlign: "right",
+                      width: "100%",
+                    }}
+                  />
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid container>
+            <Grid item xs={12}>
+              <TextField
+                variant="filled"
+                label="توضیحات"
+                style={{
+                  direction: "rtl",
+                  textAlign: "right",
+                  width: "100%",
+                }}
+                multiline
+                minRows={5}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button />
             </Grid>
           </Grid>
         </form>
