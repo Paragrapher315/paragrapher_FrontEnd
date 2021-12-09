b9677389f3f45eadd25edb5e86176ba9ace7cd7e
Hediyeh-Eshaqi <hediyeh.eshaqi@gmail.com>
2021-12-09 18:20:28 +0330

    BookSearchList added

---
+++ paragrapher/src/components/Search/BookSearchList.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Paragraph from "../Paragraph/Paragraph";
+class BookSearchList extends React.Component{
+    
+    render(){
+        return(
+            <div className="row">
+                {this.props.items.map(item => (
+                    
+                <Paragraph
+                user={item.user_name}
+                isPotd={false}
+                date={item.date}
+                avatar="ا"
+                canAction={true}
+                text={item.p_text}
+                author={item.author}
+                tags={item.tags.split(",")}
+                p_id={item.id}
+                book={item.ref_book}
+                isMine={false}
+                communityName={item.community_name}
+                />
+                    
+                    
+                
+                ))}
+            </div>
+           
+            
+        );
+    }
+}
+export default BookSearchList;
