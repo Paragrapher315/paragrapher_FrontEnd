678f7acb017da90c88ae8a5d08f44ef80c260163
Hediyeh-Eshaqi <hediyeh.eshaqi@gmail.com>
2021-12-19 22:45:40 +0330

    ProfileShow started

---
+++ paragrapher/src/components/Profile/ProfileShow.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+class ProfileShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    };
+  }
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return (
+      <div className="div"></div>
+    );
+  }
+}
+export default ProfileShow;
