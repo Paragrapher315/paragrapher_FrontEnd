ace8810cb0788942981e60c051bfaa95c5a6f3b5
Alibik315 <46296895+Alibik315@users.noreply.github.com>
2023-04-22 07:04:19 -0700

    new discount code

--- paragrapher/src/components/BuyCredits/DiscountCode.jsx
+++ paragrapher/src/components/BuyCredits/DiscountCode.jsx
@@ -2,45 +2,57 @@ import React, { useState } from "react";
 import { Button } from "@material-ui/core";
 
 
-const DiscountForm = ({ updateBalance }) => {
-    const [discountCode, setDiscountCode] = useState("");
-    const [errorMessage, setErrorMessage] = useState("");
+const DiscountForm = () => {
 
-    const handleDiscountCodeChange = (e) => {
-        setDiscountCode(e.target.value);
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        // TODO: validate discount code
-        const isValidDiscountCode = true;
-
-        if (isValidDiscountCode) {
-            // TODO: update user balance with discount
-            updateBalance();
-
-            setErrorMessage("");
-            setDiscountCode("");
+    const [credit, setCredit] = useState();
+    const [discountCode, setDiscountcode] = useState('');
+    const [error, setError] = useState('');
+    const applyDiscount = () => {
+        if (discountCode === 'BEANIE') {
+            setCredit(credit + 10000);
+            setError('');
         } else {
-            setErrorMessage("کد تخفیف اشتباه است");
+            setError('کد تخفیف اشتباه است')
         }
     };
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form >
             <label>
                 کد تخفیف:
                 <input
-                    style={{textAlign:"center", width: "50%",height:'40px', paddingTop: "2vh" ,marginTop:"2vh",paddingTop:"0.1vh",backgroundColor:"#e2e2e2",border:"none"}}
+                    style={
+                        {
+                            textAlign: "center",
+                            width: "50%",
+                            height: '40px',
+                            paddingTop: "2vh",
+                            marginTop: "2vh",
+                            paddingTop: "0.1vh",
+                            backgroundColor: "#e2e2e2",
+                            border: "none"
+                        }}
                     variant="filled"
                     type="text"
                     value={discountCode}
-                    onChange={handleDiscountCodeChange}
+                    onChange={(e) => setDiscountcode(e.target.value)}
                 />
             </label>
-            <Button color="secondary" variant="contained" style={{borderRadius:"5px",padding:"8px",}} type="submit">اعمال کد تخفیف</Button>
-            {errorMessage && <div className="error">{errorMessage}</div>}
+            <Button onClick={applyDiscount} color="secondary" variant="contained" style={
+                {
+                    borderRadius: "5px",
+                    padding: "8px",
+                }}
+
+                type="submit">اعمال کد تخفیف</Button>
+
+            <h2>{credit}</h2>
+            {error && <p
+                style={{
+                    color: 'red'
+
+                }}
+                className="error">{error}</p>}
         </form>
     );
 };
